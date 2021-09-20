cb4e6254e091f86bf332fda093447fd14489fe7d
Lucas Jeub <lucasjeub@gmail.com>
2021-09-20 20:56:52 +0200

    don't copy data and add debug output

--- local2global_embedding/run/scripts/l2g_align_patches.py
+++ local2global_embedding/run/scripts/l2g_align_patches.py
@@ -24,6 +24,7 @@ import torch
 import numpy as np
 from numpy.lib.format import open_memmap
 from filelock import SoftFileLock
+import tqdm
 
 from local2global import WeightedAlignmentProblem, Patch
 from local2global_embedding.run.utils import ScriptParser
@@ -41,32 +42,38 @@ def main(patch_folder: str, basename: str, dim: int, mmap=False):
     mmap_load = 'r' if mmap else None
     for criterion in criterions:
         with SoftFileLock(patch_folder / f'{basename}_d{dim}_{criterion}_coords.lock', timeout=10):  # only one task at a time
-            for i in range(patch_graph.num_nodes):
+            print('loading patch data')
+            for i in tqdm(range(patch_graph.num_nodes)):
                 patch_file = patch_folder / f'patch{i}_data.pt'
                 patch = torch.load(patch_file, map_location='cpu')
-                with SoftFileLock(f'{basename}_patch{i}_info.lock'):
+                with SoftFileLock(f'{basename}_patch{i}_info.lock', timeout=10):
                     coords = np.load(patch_folder / f'{basename}_patch{i}_d{dim}_best_{criterion}_coords.npy',
                                      mmap_mode=mmap_load)
                 patch_list.append(Patch(patch.nodes, coords))
 
-            prob = WeightedAlignmentProblem(patch_list, patch_edges=patch_graph.edges())
+            print('initialising alignment problem')
+            prob = WeightedAlignmentProblem(patch_list, patch_edges=patch_graph.edges(), copy_data=False)
             patched_embedding_file = patch_folder / f'{basename}_d{dim}_{criterion}_coords.npy'
             patched_embedding_file_nt = patch_folder / f'{basename}_d{dim}_{criterion}_ntcoords.npy'
             if mmap:
+                print('computing ntcoords using mmap')
                 out = open_memmap(patched_embedding_file_nt, mode='w+', shape=(prob.n_nodes, prob.dim),
                                   dtype=np.float32)
                 prob.mean_embedding(out)
                 out.flush()
             else:
+                print('computing ntcoords')
                 out = np.empty(shape=(prob.n_nodes, prob.dim), dtype=np.float32)
                 ntcoords = prob.mean_embedding(out)
                 np.save(patched_embedding_file_nt, ntcoords)
 
             if mmap:
+                print('computing aligned coords using mmap')
                 out = open_memmap(patched_embedding_file, mode='w+', shape=(prob.n_nodes, prob.dim), dtype=np.float32)
                 prob.align_patches().mean_embedding(out)
                 out.flush()
             else:
+                print('computing aligned coords')
                 out = np.empty(shape=(prob.n_nodes, prob.dim), dtype=np.float32)
                 coords = prob.align_patches().mean_embedding(out)
                 np.save(patched_embedding_file, coords)
