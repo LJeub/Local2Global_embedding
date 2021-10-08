d7d3d9c744f06675b063add9b1d6da5fb27bb7ef
Lucas Jeub <lucasjeub@gmail.com>
2021-10-08 14:25:53 +0200

    FilePatch update means that we no longer need to always copy patch data

--- local2global_embedding/run/scripts/l2g_align_patches.py
+++ local2global_embedding/run/scripts/l2g_align_patches.py
@@ -51,11 +51,12 @@ def l2g_align_patches(patch_folder: str, basename: str, dim: int, criterion: str
                 patch = torch.load(patch_file, map_location='cpu')
                 nodes = patch.nodes
             if mmap:
-                coords = np.load(coords_file, mmap_mode='r')
-                coords_file = NamedTemporaryFile(delete=False)
-                np.save(coords_file, coords)
-                coords_file.close()
-                coords_file = coords_file.name
+                if use_tmp:
+                    coords = np.load(coords_file, mmap_mode='r')
+                    coords_file = NamedTemporaryFile(delete=False)
+                    np.save(coords_file, coords)
+                    coords_file.close()
+                    coords_file = coords_file.name
                 patch_list.append(FilePatch(nodes, coords_file))
             else:
                 with SoftFileLock(f'{basename}_patch{i}_info.lock', timeout=10):
