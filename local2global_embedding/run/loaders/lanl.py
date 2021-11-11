6977de14db2066f0c0ac6f9b75d9a27b2577ebd2
Lucas Jeub <lucasjeub@gmail.com>
2021-11-11 14:22:03 +0100

    implement basic loading of LANL data

--- local2global_embedding/run/loaders/lanl.py
+++ local2global_embedding/run/loaders/lanl.py
@@ -17,7 +17,31 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
-from . import planetoid
-from . import amazon
-from . import mag240m
-from . import lanl
+from pathlib import Path
+
+from pandas import DataFrame, read_csv
+import numpy as np
+import scipy.sparse as ss
+
+from local2global_embedding.run.utils import dataloader
+
+fields= ('source_id', 'dest_id', 'total_time', 'src_packets', 'dest_packets', 'src_bytes', 'dst_bytes')
+
+
+def _build_adj(data: DataFrame, weight=None, weight_transform=None):
+    if weight is None:
+        weight = np.broadcast_to(np.ones(1), (data.shape[0],))
+    else:
+        weight = data[weight]
+
+    if weight_transform is not None:
+        weight = weight.apply(weight_transform)
+
+    return ss.coo_matrix((weight, (data['source_id'], data['dest_id'])))
+
+
+@dataloader('lanl')
+def _load_data(root, days, protocol='TCP', weight=None, weight_transform=None):
+    root = Path(root)
+    data = (read_csv(root / f'netflow_day-{day:02}_aggregate_{protocol}.csv', names=fields) for day in days)
+    return (_build_adj(d, weight, weight_transform) for d in data)
