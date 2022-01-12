bae964786dfd1ea2cf893bc004e296e6d108b171
Lucas Jeub <lucasjeub@gmail.com>
2022-01-12 21:20:35 +0000

    updated embedding plot

--- local2global_embedding/run/umap_plot.py
+++ local2global_embedding/run/umap_plot.py
@@ -39,7 +39,16 @@ from local2global_embedding.run.utils import ScriptParser, load_classification_p
 rng = np.random.default_rng()
 
 
-def plot_embedding(filename, name, mmap_mode: Optional[str] = None, max_points=50000, restrict_lcc=False,
+def get_ax_size(ax):
+    fig = ax.figure
+    bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    width, height = bbox.width, bbox.height
+    width *= fig.dpi
+    height *= fig.dpi
+    return width, height
+
+
+def plot_embedding(filename, name, mmap_mode: Optional[str] = None, max_points=500000, restrict_lcc=False,
                    pointsize=5,
                    size=2.0, dpi=1200, data_root='/tmp', min_dist=0.0, metric='euclidean', verbose=True):
     filename = Path(filename)
@@ -48,6 +57,8 @@ def plot_embedding(filename, name, mmap_mode: Optional[str] = None, max_points=5
     print(f'classificaton problem loaded at {datetime.now()}')
     fig = plt.figure(figsize=(size, size), dpi=dpi)
     ax = fig.add_axes([0, 0, 1, 1])
+    ax_size = size*dpi
+    pad = 2*pointsize / ax_size
     y = np.asanyarray(cl.y)
     nodes = np.flatnonzero(y >= 0)
     if len(nodes) > max_points:
@@ -58,13 +69,21 @@ def plot_embedding(filename, name, mmap_mode: Optional[str] = None, max_points=5
         coords = np.load(filename, mmap_mode=mmap_mode)[nodes]
     print(f'embedding loaded at {datetime.now()}')
     vc = umap.UMAP(min_dist=min_dist, metric=metric, verbose=verbose).fit_transform(coords)
+    min_range = vc.min(axis=0)
+    max_range = vc.max(axis=0)
+    pad = (max_range-min_range) * pad
+
+    x_range = (min_range[0]-pad[0], max_range[0]+pad[0])
+    y_range = (min_range[1]-pad[1], max_range[1]+pad[1])
+
     df = pd.DataFrame(vc, columns=['x', 'y'])
     df['label'] = y[nodes]
     df['label'] = df['label'].astype('category')
     colors = sns.color_palette('husl', cl.num_labels)
     colors = {i: tuple(int(vi * 255) for vi in v) for i, v in enumerate(colors)}
     dsshow(df, ds.Point('x', 'y'), ds.count_cat('label'), ax=ax, norm='eq_hist', color_key=colors,
-           shade_hook=partial(tf.dynspread, threshold=0.99, max_px=pointsize, shape='circle'), alpha_range=(55, 255))
+           shade_hook=partial(tf.dynspread, threshold=0.99, max_px=pointsize, shape='circle'), alpha_range=(55, 255),
+           x_range=x_range, y_range=y_range)
 
     ax.set_axis_off()
     plt.margins(0.01, 0.01)
