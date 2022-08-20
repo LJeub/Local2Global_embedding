951d066695458686f232db6ae7d5b9541407cd91
Lucas Jeub <lucasjeub@gmail.com>
2022-08-20 16:26:42 +0200

    fix memory pinning for data that is already on the gpu

--- local2global_embedding/classfication.py
+++ local2global_embedding/classfication.py
@@ -306,7 +306,7 @@ def train(data: ClassificationProblem, model: torch.nn.Module, num_epochs, batch
                                  eps=adam_epsilon)
     # optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay)
     data_loader = torch.utils.data.DataLoader(BatchedData(train_data, batch_size=batch_size), batch_size=1,
-                                              shuffle=True, pin_memory=True, collate_fn=lambda b: b[0])
+                                              shuffle=True, pin_memory=not train_data.x.is_cuda, collate_fn=lambda b: b[0])
     if decay_lr:
         lr_sched = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(data_loader) * num_epochs)
 
